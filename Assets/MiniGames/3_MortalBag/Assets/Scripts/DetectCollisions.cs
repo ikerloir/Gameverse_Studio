4661ba79885c1e69a7fba421fc1d0df8db9546a7
ikerloir <163097505+ikerloir@users.noreply.github.com>
2025-05-15 16:05:00 +0200

    arreglo de errores de compilacion
    delete de archivos duplicados extras text mesh pro
    modificación de nombres de script duplicados.
    modificaciones de referencias en otros scripts de los nombre duplicados
    actualización de scena intro juego edu

--- Assets/MiniGames/3_MortalBag/Assets/Scripts/DetectCollisions.cs
+++ Assets/MiniGames/3_MortalBag/Assets/Scripts/DetectCollisions.cs
@@ -3,24 +3,24 @@ using UnityEngine;
 
 public class DetectCollisions : MonoBehaviour
 {
-    public int points = 10; 
+    public int points = 10;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     private void OnTriggerEnter(Collider other)
     {
-          
-       
+
+
         Destroy(other.gameObject);
-        ScoreManager.Instance.AddScore(points);
+        MortalBagScoreManager.Instance.AddScore(points);
 
     }
 }
